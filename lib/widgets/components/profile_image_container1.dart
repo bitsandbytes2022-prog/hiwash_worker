75647036947cc9f474f186de21c55b57995597d8
expertamitch <expertamitch@gmail.com>
2025-06-09 15:51:56 +0530

    apply string constant

---
+++ lib/widgets/components/profile_image_container1.dart
@@ -0,0 +1,82 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/material.dart';
+import 'package:hiwash_worker/widgets/sized_box_extension.dart';
+
+import '../../generated/assets.dart';
+import '../../styling/app_color.dart';
+import 'image_view.dart';
+
+class ProfileImageView extends StatelessWidget {
+  final double? height;
+  final double? width;
+  final double? radius;
+  final String? imagePath;
+  final String? image;
+  final double? heightStack;
+  final double? widthStack;
+  final double? radiusStack;
+  final bool? isVisibleStack;
+  final bool? isTopRight;
+
+  const ProfileImageView({
+    super.key,
+    this.height = 40,
+    this.width = 40,
+    this.radius = 20,
+    this.imagePath,
+    this.image,
+    this.heightStack = 15,
+    this.widthStack = 15,
+    this.radiusStack = 10,
+    this.isVisibleStack = true,
+    this.isTopRight,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      // alignment: isTopRight==true?Alignment.topRight:Alignment.topLeft,
+      children: [
+        Container(
+          padding: EdgeInsets.all(4),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(100),
+            border: Border.all(color: AppColor.blue.withOpacity(0.2)),
+          ),
+          child: CircleAvatar(
+            radius: radius,
+            backgroundColor: Colors.grey[200],
+            child: ClipOval(
+              child: CachedNetworkImage(
+                imageUrl: imagePath ?? '',
+                placeholder:
+                    (context, url) => SizedBox(
+                  height: 20,
+                  width: 20,
+                  child: CircularProgressIndicator(strokeWidth: 4),
+                ),
+                errorWidget:
+                    (context, url, error) =>
+                    Image.asset(Assets.imagesDemoProfile),
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+        ),
+        if (isVisibleStack == true)
+          Container(
+            padding: EdgeInsets.all(3),
+            decoration: BoxDecoration(
+              color: AppColor.white,
+              borderRadius: BorderRadius.circular(100),
+              border: Border.all(color: AppColor.cE8E9F4),
+            ),
+            child: CircleAvatar(
+              radius: radiusStack,
+              backgroundImage: AssetImage(image ?? Assets.iconsIcCrown),
+            ),
+          ),
+      ],
+    );
+  }
+}
